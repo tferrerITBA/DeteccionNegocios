3439785cbe8239885a5451f247148e82408e136c
tferrerITBA <tferrer@itba.edu.ar>
2017-06-19 15:45:42 -0300

    Se agregó Asset

---
+++ CarteraDeInversion/src/com/carteradeinversion/sys/Asset.java
@@ -0,0 +1,16 @@
+package com.carteradeinversion.sys;
+
+public class Asset {
+	
+	private double val;
+	private double min;
+	private double max;
+	private double open;
+	private double close;
+	private double variation;
+	private int amount;
+	private String name;
+	private String ticker;
+	
+	
+}
